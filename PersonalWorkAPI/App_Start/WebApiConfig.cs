28914d2ebf015df3bf5b250771bac21895180410
kouson <kouson@163.com>
2017-11-29 11:37:42 +0800

    提交代码

---
+++ PersonalWorkAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace PersonalWorkAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            //跨域配置
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+
+            ////同一个ApiController 出现多个get或者post等调用提示
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = RouteParameter.Optional }
+             );
+
+            // 取消注释下面的代码行可对具有 IQueryable 或 IQueryable<T> 返回类型的操作启用查询支持。
+            // 若要避免处理意外查询或恶意查询，请使用 QueryableAttribute 上的验证设置来验证传入查询。
+            // 有关详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=279712。
+            //config.EnableQuerySupport();
+
+            // 若要在应用程序中禁用跟踪，请注释掉或删除以下代码行
+            // 有关详细信息，请参阅: http://www.asp.net/web-api
+            config.EnableSystemDiagnosticsTracing();
+        }
+    }
+}
---
+++ PersonalWorkAPI/Controllers/ArticleController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Collections;
+using System.Configuration;
+using System.Web;
+using System.IO;
+using System.Globalization; 
+
+namespace PersonalWorkAPI.Controllers
+{
+    public class ArticleController : ApiController
+    { 
+        [AcceptVerbs("GET", "POST")] 
+        public void upLoadDocImage()
+        {
+            HttpContext context = HttpContext.Current; 
+            context.Response.ContentEncoding = System.Text.Encoding.UTF8;
+            //上传配置
+            string pathbase = "upload/";                                                          //保存路径
+            int size = 10;                     //文件大小限制,单位mb                                                                                   //文件大小限制，单位KB
+            string[] filetype = { ".gif", ".png", ".jpg", ".jpeg", ".bmp" };                    //文件允许格式
+
+            string callback = context.Request["callback"];
+            string editorId = context.Request["editorid"];
+
+            //上传图片
+            Hashtable info;
+            Uploader up = new Uploader();
+            info = up.upFile(context, pathbase, filetype, size); //获取上传状态
+            string json = BuildJson(info);
+
+            context.Response.ContentType = "text/html";
+            if (callback != null)
+            {
+                context.Response.Write(String.Format("<script>{0}(JSON.parse(\"{1}\"));</script>", callback, json));
+            }
+            else
+            {
+                context.Response.Write(json);
+            }
+             
+        }
+        private string BuildJson(Hashtable info)
+        {
+            List<string> fields = new List<string>();
+            string[] keys = new string[] { "originalName", "name", "url", "size", "state", "type" };
+            for (int i = 0; i < keys.Length; i++)
+            {
+                fields.Add(String.Format("\"{0}\": \"{1}\"", keys[i], info[keys[i]]));
+            }
+            return "{" + String.Join(",", fields) + "}";
+        }
+    }
+}
