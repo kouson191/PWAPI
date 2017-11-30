7feaa79a008823b89430fc4d86f5485a98c991c5
Kouson <kouson@163.com>
2017-11-30 23:19:53 +0800

    增加登录用户验证

---
+++ PersonalWorkAPI/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Collections;
+using System.Configuration;
+using System.Web;
+using System.IO;
+using System.Globalization;
+using PersonalWorkAPI.DAL;
+using PersonalWorkAPI.Model;
+using PersonalWorkAPI.Common;
+using System.Text; 
+
+namespace PersonalWorkAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        private readonly PersonalWorkAPI.DAL.User dal = new PersonalWorkAPI.DAL.User();
+
+        [HttpGet]
+        public HttpResponseMessage verifyUser(string user_name, string user_pwd)
+        {
+            ReturnBaseObject<Boolean> returnObj = new ReturnBaseObject<Boolean>();
+            try
+            {
+                returnObj.ReturnObject = dal.verifyUser(user_name, user_pwd);
+
+                returnObj.IsError = false;
+
+            }
+            catch (Exception ex)
+            {
+                returnObj.IsError = true;
+                returnObj.Error.ErrorMsg = ex.Message.ToString();
+                returnObj.Error.ErrorCode = Error.EnumErrorCode.未知错误;
+            }
+
+            string str = Jil.JSON.Serialize<ReturnBaseObject<Boolean>>(returnObj);
+            HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent(str, Encoding.GetEncoding("UTF-8"), "application/json") };
+            return result;
+        }
+
+    }
+}
---
+++ PersonalWorkAPI/DAL/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+using PersonalWorkAPI.Common;
+using PersonalWorkAPI.Model;
+using System.Collections.Generic;
+
+namespace PersonalWorkAPI.DAL
+{
+    /// <summary>
+    /// 数据访问类:article
+    /// </summar 
+    public class User
+    {
+        public Boolean verifyUser(string user_name, string user_pwd)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("select count(1) from user");
+            strSql.Append(" where user_name=@user_name and  user_pwd=@user_pwd ");
+            MySqlParameter[] parameters = {
+					new MySqlParameter("@user_name", MySqlDbType.String),
+					new MySqlParameter("@user_pwd", MySqlDbType.String),
+			};
+            parameters[0].Value = user_name;
+            parameters[1].Value = user_pwd;
+
+            return DbHelperMySQL.Exists(strSql.ToString(), parameters);
+        
+        }
+
+    }
+}
